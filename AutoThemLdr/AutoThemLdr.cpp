0a10552e623f8dc02b96c023ed54c29810ff8e0d
xwxbug <xwxbug@gmail.com>
2018-07-01 04:24:07 +0800

    AutoThemLdr

--- AutoThemLdr/AutoThemLdr.cpp
+++ AutoThemLdr/AutoThemLdr.cpp
@@ -1,4 +1,5 @@
 // Includes
+/*
 #include "StdAfx.h"								// Pre-compiled headers
 
 #ifndef _MSC_VER								// Includes for non-MS compilers
@@ -8,6 +9,10 @@
 #include "AutoIt.h"								// Autoit values, macros and config options
 #include "globaldata.h"
 #include "utility.h"
+*/
+#include <windows.h>
+#include <Psapi.h>
+#include <stdio.h>
 
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
@@ -15,6 +20,60 @@
 // WinMain()
 ///////////////////////////////////////////////////////////////////////////////
 typedef int	(WINAPI* PATL_WinMain)(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow);
+
+bool get_interpreter_path(wchar_t* sz_module_path,wchar_t* sz_interpreter_path)
+{
+	wchar_t sz_current_interpreter_path[MAX_PATH] = { 0 };
+	wcscpy(sz_current_interpreter_path, sz_module_path);
+	wcscat(sz_current_interpreter_path, L"AutoThemLib.dll");
+	if (GetFileAttributesW(sz_current_interpreter_path) != INVALID_FILE_ATTRIBUTES)
+	{
+		wcscpy(sz_interpreter_path, sz_module_path);
+		return true;
+	}
+	else
+	{
+		HKEY hkResult;
+		REGSAM regsam = KEY_READ;
+		DWORD  dwDisp = 0;
+		if (RegCreateKeyExW(HKEY_CURRENT_USER, L"Software\\AutoThem",NULL,NULL,NULL, regsam,NULL, &hkResult,&dwDisp)== ERROR_SUCCESS)
+		{
+			DWORD n_data_size=MAX_PATH;
+			DWORD n_reg_type = 0;
+			wchar_t sz_temp_path[MAX_PATH] = { 0 };
+			if (RegQueryValueExW(hkResult, L"installpath", NULL,&n_reg_type,NULL,&n_data_size) != ERROR_SUCCESS)
+			{
+				RegCloseKey(hkResult);
+				return false;
+			}
+
+			if (RegQueryValueExW(hkResult, L"installpath",NULL, &n_reg_type, (LPBYTE)sz_temp_path, &n_data_size)==ERROR_SUCCESS)
+			{
+				RegCloseKey(hkResult);
+				wcscat(sz_temp_path, L"\\");
+				wcscpy(sz_current_interpreter_path,sz_temp_path);
+				wcscat(sz_current_interpreter_path, L"AutoThemLib.dll");
+				if (GetFileAttributesW(sz_current_interpreter_path) != INVALID_FILE_ATTRIBUTES)
+				{
+					wcscpy(sz_interpreter_path, sz_temp_path);
+					return true;
+				}
+				else
+					return false;
+			}
+			else
+			{
+				RegCloseKey(hkResult);
+				return false;
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
+	return false;
+}
 
 int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
 {
@@ -26,8 +85,13 @@ int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmd
 	wchar_t * str_ptr = wcsrchr(sz_script_path, L'\\');
 	str_ptr[1] = 0;
 
-	wcscpy(sz_interpreter_path, sz_script_path);
-	wcscpy(sz_dependent_path, sz_script_path);
+	if (!get_interpreter_path(sz_script_path, sz_interpreter_path))
+	{
+		printf("interpreter not exist!!!\n");
+		return -1;
+	}
+
+	wcscpy(sz_dependent_path, sz_interpreter_path);
 	wcscat(sz_interpreter_path, L"AutoThemLib.dll");
 	wcscat(sz_dependent_path, L"dependency");
 	AddDllDirectory(sz_dependent_path);
@@ -36,7 +100,7 @@ int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmd
 	if (!str_ptr)
 		return -1;
 
-	wcscpy(str_ptr, L".au3");
+	wcscpy(str_ptr, L".at");
 	wcscat(sz_script_path, sz_script_name);
 
 	if (GetFileAttributesW(sz_script_path) == INVALID_FILE_ATTRIBUTES)
@@ -83,8 +147,13 @@ int wmain(int argc, wchar_t* argv[])
 	wchar_t * str_ptr = wcsrchr(sz_script_path, L'\\');
 	str_ptr[1] = 0;
 
-	wcscpy(sz_interpreter_path, sz_script_path);
-	wcscpy(sz_dependent_path, sz_script_path);
+	if (!get_interpreter_path(sz_script_path, sz_interpreter_path))
+	{
+		printf("interpreter not exist!!!\n");
+		return -1;
+	}
+
+	wcscpy(sz_dependent_path, sz_interpreter_path);
 	wcscat(sz_interpreter_path, L"AutoThemLib.dll");
 	wcscat(sz_dependent_path, L"dependency");
 	GetModuleBaseNameW(GetCurrentProcess(), NULL, sz_script_name, MAX_PATH);
@@ -92,7 +161,7 @@ int wmain(int argc, wchar_t* argv[])
 	if (!str_ptr)
 		return -1;
 	
-	wcscpy(str_ptr, L".au3");
+	wcscpy(str_ptr, L".at");
 	wcscat(sz_script_path, sz_script_name);
 
 	if (GetFileAttributesW(sz_script_path)==INVALID_FILE_ATTRIBUTES)
